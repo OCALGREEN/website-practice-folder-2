2c432234530790793f0e6d34e9007d741d23e449
Oger Carrillo <ogercarrillo@gmail.com>
2023-03-28 15:12:50 -0700

    fixed useHistory error

--- client/src/views/Create.jsx
+++ client/src/views/Create.jsx
@@ -1,9 +1,43 @@
-import Reacte from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+
 
 const Create = () => {
+
+    const [title, setTitle] = useState("") 
+    const [price, setPrice] = useState(0) 
+    const [description, setDescription] = useState("") 
+    const history = useHistory() 
+
+    const handleSubmit =(e) => {
+        e.preventDefault() 
+        axios.post(`http://localhost:8000/product/createOne`)
+            .then(res => {
+
+            }) 
+            .catch(err => console.log(err)) 
+    }
+
+
+
     return (
         <div>
-            <h1>HELLO</h1>
+            <form>
+                <div>
+                    <label>Title</label>
+                    <input type="text" name="title" value={title} onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input type="number" name="price" value={price} onChange={e => setPrice(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input type="text" name="description" value={description} onChange={e => setDescription(e.target.value)}/>
+                </div>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
